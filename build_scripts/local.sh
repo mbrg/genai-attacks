e76280e5c986edb69addd54254477562532e1fcb
mbrg <11074433+mbrg@users.noreply.github.com>
2024-09-27 03:38:37 -0700

    use custom theme for analytics

--- build_scripts/local.sh
+++ build_scripts/local.sh
@@ -1,3 +1,9 @@
+# generate markdown from json files
 python build_scripts/generate_content_as_md.py
+# customize book theme
+mkdir -p build/theme
+cp book_theme/head.hbs build/theme/head.hbs
+# build book
 bin/mdbook build
+# rewrite book URLs
 python build_scripts/rewrite_mdbook_links.py --book-dir book/
