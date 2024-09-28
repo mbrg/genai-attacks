57a824f24a6fa229bc0268f147b9692e72f4d870
mbrg <11074433+mbrg@users.noreply.github.com>
2024-09-28 16:05:48 +0300

    descriptive error message

--- tests/file_name_test.py
+++ tests/file_name_test.py
@@ -7,4 +7,4 @@ def test_object_file_name_and_id_align(json_object_path):
     obj = load_json_object(json_object_path)
     assert (
         obj["$id"].split("/")[-1] == json_object_path.split("/")[-1].split(".")[0]
-    ), "Object $id should align to its filename"
+    ), "Object $id should align to its filename. For example, an object with id $gai-technique/example should have file name example.json."
