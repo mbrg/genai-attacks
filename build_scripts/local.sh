a6a92dd423aaaea214266fca8059876892b57336
mbrg <11074433+mbrg@users.noreply.github.com>
2024-09-28 10:39:31 +0100

    echo book location for easy access

--- build_scripts/local.sh
+++ build_scripts/local.sh
@@ -1,3 +1,5 @@
 python build_scripts/generate_content_as_md.py
 bin/mdbook build
 python build_scripts/rewrite_mdbook_links.py --book-dir book/
+# echo book location for easy access
+echo "book/index.html"
