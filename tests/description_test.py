b7a2aa5a2848ce576932a4188a778850b646f29d
mbrg <11074433+mbrg@users.noreply.github.com>
2024-09-28 16:27:16 +0100

    fix test string

--- tests/description_test.py
+++ tests/description_test.py
@@ -16,5 +16,5 @@ def test_multi_line_code_block_new_line_wrappers(obj):
     for _, code_block in zip(
         split_by_long_code_block[0::2], split_by_long_code_block[1::2]
     ):
-        assert code_block[0] == "\n", "Multi-line code block must start with \\n"
-        assert code_block[-1] == "\n", "Multi-line code block must end with \\n"
+        assert code_block[0] == "\n", "Multi-line code block must start with newline"
+        assert code_block[-1] == "\n", "Multi-line code block must end with newline"
