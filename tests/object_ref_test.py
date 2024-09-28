eece8a4c798eed7abfe224911b76a468baef86cb
mbrg <11074433+mbrg@users.noreply.github.com>
2024-09-28 15:57:21 +0300

    test_reference_is_valid

---
+++ tests/object_ref_test.py
@@ -0,0 +1,12 @@
+import pytest
+from test_utils import *
+
+
+@pytest.mark.parametrize("json_object_path", OBJECT_FILE_NAMES)
+@load_json_object
+def test_reference_is_valid(obj):
+    for ref in obj.get("object_references", []):
+        ref_file_name = ref["$id"].replace("$gai-", "") + ".json"
+        assert (
+            ref_file_name in OBJECT_FILE_NAMES
+        ), f"Object {obj['$id']} has invalid reference to {ref['$id']}"
