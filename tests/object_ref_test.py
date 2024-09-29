da96619ed7d74fbc2832a06834969ce147941040
mbrg <11074433+mbrg@users.noreply.github.com>
2024-09-29 12:02:34 +0300

    sub_technique_of

--- tests/object_ref_test.py
+++ tests/object_ref_test.py
@@ -2,11 +2,25 @@ import pytest
 from test_utils import *
 
 
+def _validate_referenced_object_id(ref_obj_id):
+    ref_file_name = ref_obj_id.replace("$gai-", "") + ".json"
+    assert (
+        ref_file_name in OBJECT_FILE_NAMES
+    ), f"Invalid reference to object id: {ref_obj_id}"
+
+
 @pytest.mark.parametrize("json_object_path", OBJECT_FILE_NAMES)
 @load_json_object_wrapper
-def test_reference_is_valid(obj):
+def test_object_references(obj):
     for ref in obj.get("object_references", []):
-        ref_file_name = ref["$id"].replace("$gai-", "") + ".json"
-        assert (
-            ref_file_name in OBJECT_FILE_NAMES
-        ), f"Object {obj['$id']} has invalid reference to {ref['$id']}"
+        _validate_referenced_object_id(ref)
+
+
+@pytest.mark.parametrize(
+    "json_object_path",
+    [file_name for file_name in OBJECT_FILE_NAMES if file_name.startswith("technique")],
+)
+@load_json_object_wrapper
+def test_sub_technique_references(obj):
+    if "sub_technique_of" in obj:
+        _validate_referenced_object_id(obj["sub_technique_of"])
